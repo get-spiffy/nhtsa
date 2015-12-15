010671356598b0f7dbdcb0356a7f82a1a7ccf1de
Mwaki Harri Magotswi <magotswi@gmail.com>
2015-12-15 15:23:43 -0500

    Adding recalls support

--- lib/nhtsa.rb
+++ lib/nhtsa.rb
@@ -1,5 +1,6 @@
 require "nhtsa/version"
 require "nhtsa/safety_ratings"
+require "nhtsa/recalls"
 
 module Nhtsa
 end
---
+++ test/recalls/models_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ModelsTest < Minitest::Test
+
+  def test_it_builds_url_as_expected
+    expected_url = "http://webapi.nhtsa.gov/api/Recalls/vehicle/modelyear/1991/make/Mitsubishi?format=json"
+
+    assert_equal expected_url, Nhtsa::Recalls::Models.new(1991, "Mitsubishi").url
+  end
+
+  def test_models_returns_an_array_of_models
+    model_names = ["3000GT", "DIAMANTE", "ECLIPSE", "ECLIPSE SPYDER", "GALANT", "MIRAGE", "MONTERO", "MONTERO SPORT", "NATIVA", "SPYDER"]
+
+    assert_equal model_names, Nhtsa::Recalls::Models.new(1998, "Mitsubishi").models
+  end
+
+end
