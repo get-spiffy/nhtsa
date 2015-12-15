8a67c3cafefd63607dff6aa2e79c4da3fed79131
Mwaki Harri Magotswi <magotswi@gmail.com>
2015-12-15 16:16:46 -0500

    some changes were left out, adding them back in

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,27 +9,27 @@ class Minitest::Test
 
   FakeWeb.register_uri(:get, "http://webapi.nhtsa.gov/api/SafetyRatings?format=json",
     :content_type => "application/json",
-    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings_years.json")).read
+    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings/years.json")).read
   )
 
   FakeWeb.register_uri(:get, "http://webapi.nhtsa.gov/api/SafetyRatings/modelyear/1998?format=json",
     :content_type => "application/json",
-    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings_manufacturers.json")).read
+    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings/manufacturers.json")).read
   )
 
   FakeWeb.register_uri(:get, "http://webapi.nhtsa.gov/api/SafetyRatings/modelyear/1998/make/Mitsubishi?format=json",
     :content_type => "application/json",
-    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings_models.json")).read
+    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings/models.json")).read
   )
 
   FakeWeb.register_uri(:get, "http://webapi.nhtsa.gov/api/SafetyRatings/modelyear/1998/make/Mitsubishi/model/Galant?format=json",
     :content_type => "application/json",
-    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings_trims.json")).read
+    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings/trims.json")).read
   )
 
   FakeWeb.register_uri(:get, "http://webapi.nhtsa.gov/api/SafetyRatings/VehicleId/3832?format=json",
     :content_type => "application/json",
-    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings_ratings.json")).read
+    :body => File.open(File.expand_path(".", "test/fixtures/safety_ratings/ratings.json")).read
   )
 
 end
