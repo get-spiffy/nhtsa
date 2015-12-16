056fd06eac913a1303dce79662febda24fa3e2a1
Mwaki Harri Magotswi <magotswi@gmail.com>
2015-12-16 01:24:36 -0500

    adding namespacing to tests

--- test/safety_ratings/years_test.rb
+++ test/safety_ratings/years_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
 
-class YearsTest < Minitest::Test
+module SafetyRatings
+  class YearsTest < Minitest::Test
 
-  def test_it_builds_url_as_expected
-    expected_url = "http://webapi.nhtsa.gov/api/SafetyRatings?format=json"
+    def test_it_builds_url_as_expected
+      expected_url = "http://webapi.nhtsa.gov/api/SafetyRatings?format=json"
 
-    assert_equal expected_url, Nhtsa::SafetyRatings::Years.new.url
+      assert_equal expected_url, Nhtsa::SafetyRatings::Years.new.url
+    end
+
+    def test_years_returns_an_array_of_years
+      years = [1998, 1997, 1996, 1993, 1992, 1991]
+
+      assert_equal years, Nhtsa::SafetyRatings::Years.new.years
+    end
+
   end
-
-  def test_years_returns_an_array_of_years
-    years = [1998, 1997, 1996, 1993, 1992, 1991]
-
-    assert_equal years, Nhtsa::SafetyRatings::Years.new.years
-  end
-
 end
