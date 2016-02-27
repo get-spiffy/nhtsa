9070a1fd071ad47e4eb8aa370ec26d1bd5b2ae5f
Mwaki Harri Magotswi <magotswi@gmail.com>
2016-02-27 13:39:46 -0500

    Adding behviour and psecs around safety_ratings/years

--- lib/nhtsa/safety_ratings/years.rb
+++ lib/nhtsa/safety_ratings/years.rb
@@ -1,12 +1,24 @@
 module Nhtsa
   module SafetyRatings
     class Years
+      def initialize
+        @years = JSON.parse(open(url).read)["Results"].collect{|year| Year.new(year["ModelYear"])}
+      end
+
       def url
         BASE_URI + END_POINT + DEFAULT_PARAMS
       end
 
       def years
-        JSON.parse(open(url).read)["Results"].collect{|year| year["ModelYear"]}
+        @years
+      end
+
+      def values
+        @years.map(&:value)
+      end
+
+      def to_s
+        @years.map(&:to_s)
       end
     end
   end
